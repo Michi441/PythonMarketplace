641469934e278e0aac9a5cb893cf5e21a18a31b2
Doris <DorisMac@doriss-mbp.speedport_w_723v_1_45_000>
2018-01-20 12:32:42 +0100

    Added views with base.html

--- sixerapp/views.py
+++ sixerapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+def gig_detail(request, id):
+    return render(request, 'gig_detail.html', {})
--- sixerr/urls.py
+++ sixerr/urls.py
@@ -19,5 +19,6 @@ from sixerapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_detail, name='gig_detail')
 ]
