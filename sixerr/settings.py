5d5d4e4176897777cf8d903315f57563ce870094
Doris <DorisMac@doriss-mbp.speedport_w_723v_1_45_000>
2018-01-21 20:23:26 +0100

    Added files for heroku deploy

--- sixerr/settings.py
+++ sixerr/settings.py
@@ -127,6 +127,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_URL = 'home'
 
